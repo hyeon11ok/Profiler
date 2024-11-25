ae2a43d0c50c661f0ba332ca9eaf95465d63e81e
hyeon11ok <hyeonsusin05@gmail.com>
2024-11-26 06:00:35 +0900

    코드 리팩토링 ItemInfomation, Item 스크립트 문서화

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 아이템의 이름, 정보를 표시해줄 이미지 정보를 담고 있는 객체
+/// </summary>
 public class Item : MonoBehaviour
 {
     private Sprite itemInfo;
